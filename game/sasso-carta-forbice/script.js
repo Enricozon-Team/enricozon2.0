8682903afe22d29165b1026d8134576d5402fb55
SoFranci <trevisanfrancesco022@gmail.com>
2023-11-12 09:30:56 +0100

    fix

--- game/sasso-carta-forbice/script.js
+++ game/sasso-carta-forbice/script.js
@@ -1,20 +1,27 @@
-function playGame(playerChoice) {
-    const choices = ['Sasso', 'carta', 'Forbice'];
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    let result;
-     if (playerChoice === computerChoice) {
-         result = "E' un pareggio!";
-     } else if (
-        
-         
-        
-         (playerChoice === 'Sasso' && computerChoice === 'forbice') ||
-         (playerChoice === 'Carta' && computerChoice === 'sasso') ||
-         (playerChoice === 'Forbice' && computerChoice === 'carta')
-      ) {
-     result = 'Hai vinto!';
-     } else {
-        result = 'Hai perso!';
-     }
-document.getElementById('result').innerHTML = `Hai scelto ${playerChoice}. Il computer ha scelto ${computerChoice}. ${result}`;
- }
+function makeChoice(playerChoice) {
+    const choices = ['sasso', 'carta', 'forbice'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    const result = determineWinner(playerChoice, computerChoice);
+
+    displayResult(result, playerChoice, computerChoice);
+}
+
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'È un pareggio!';
+    } else if (
+        (playerChoice === 'sasso' && computerChoice === 'forbice') ||
+        (playerChoice === 'carta' && computerChoice === 'sasso') ||
+        (playerChoice === 'forbice' && computerChoice === 'carta')
+    ) {
+        return 'Hai vinto!';
+    } else {
+        return 'Hai perso!';
+    }
+}
+
+function displayResult(result, playerChoice, computerChoice) {
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = `<p>${result}</p><p>Hai scelto ${playerChoice}. Il Computer ha scelto ${computerChoice}.</p>`;
+}
