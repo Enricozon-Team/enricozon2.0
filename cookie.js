dddb6d623cc108ccbcc521449f1e162f86e26e1e
SoFranci <trevisanfrancesco022@gmail.com>
2023-12-17 08:41:53 +0100

    robettina

---
+++ cookie.js
@@ -0,0 +1,16 @@
+    if (localStorage.getItem('cookiesAccepted') !== 'true') {
+        document.getElementById('cookie-popup').style.display = 'block';
+    }
+
+    function accettaCookie() {
+        localStorage.setItem('cookiesAccepted', 'true');
+        document.getElementById('cookie-popup').style.display = 'none';
+    }
+
+    function rifiutaCookie() {
+        document.getElementById('cookie-popup').style.display = 'none';
+    }
+
+    function vaiAllaPaginaInfo() {
+        window.location.href = 'cookie/index.html';
+    }
