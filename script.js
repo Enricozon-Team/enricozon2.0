9898832b85c3e9f88095c48a4fe15922173799c7
SoFranci <trevisanfrancesco022@gmail.com>
2023-12-23 19:15:32 +0100

    clean cpde

--- script.js
+++ script.js
@@ -1,51 +1,48 @@
 function createSnowflake() {
-    const snowflake = document.createElement("div");
-    snowflake.className = "snowflake";
-    snowflake.style.left = `${Math.random() * window.innerWidth}px`;
-    snowflake.style.animationDuration = `${Math.random() * 2 + 3}s`; // Random duration between 3 and 5 seconds
-    snowflake.style.animationDelay = `${Math.random() * 2}s`; // Random delay between 0 and 2 seconds
+  const snowflake = document.createElement("div");
+  snowflake.className = "snowflake";
+  snowflake.style.left = `${Math.random() * window.innerWidth}px`;
+  snowflake.style.animationDuration = `${Math.random() * 2 + 3}s`; // Random duration between 3 and 5 seconds
+  snowflake.style.animationDelay = `${Math.random() * 2}s`; // Random delay between 0 and 2 seconds
 
-    document.body.appendChild(snowflake);
+  document.body.appendChild(snowflake);
 
-    // Remove the snowflake when the animation is complete
-    snowflake.onanimationend = () => {
-        snowflake.remove();
-    };
+  // Remove the snowflake when the animation is complete
+  snowflake.onanimationend = () => {
+    snowflake.remove();
+  };
 }
 
 // Function to create multiple snowflakes
 function createSnowfall() {
-    const numSnowflakes = 7; // Number of snowflakes to generate
-    for (let i = 0; i < numSnowflakes; i++) {
-        createSnowflake();
-    }
+  const numSnowflakes = 7; // Number of snowflakes to generate
+  for (let i = 0; i < numSnowflakes; i++) {
+    createSnowflake();
+  }
 }
 
 // Generate snowfall every second
 setInterval(createSnowfall, 2000);
 
+document.addEventListener("DOMContentLoaded", function () {
+  const giftBox = document.getElementById("gift-box");
 
+  // Function to show the gift box
+  function showGiftBox() {
+    giftBox.style.display = "block";
 
-document.addEventListener('DOMContentLoaded', function() {
-    const giftBox = document.getElementById('gift-box');
-  
-    // Function to show the gift box
-    function showGiftBox() {
-      giftBox.style.display = 'block';
-  
-      // Set a timeout to hide the gift box after 2 seconds
-      setTimeout(function() {
-        giftBox.style.display = 'none';
-      }, 2000);
-    }
-  
-    // Event listener for when the user clicks on the page
-    document.addEventListener('click', function(event) {
-      // Show the gift box at the clicked position
-      giftBox.style.left = `${event.clientX}px`;
-      giftBox.style.top = `${event.clientY}px`;
-  
-      showGiftBox();
-    });
+    // Set a timeout to hide the gift box after 2 seconds
+    setTimeout(function () {
+      giftBox.style.display = "none";
+    }, 2000);
+  }
+
+  // Event listener for when the user clicks on the page
+  document.addEventListener("click", function (event) {
+    // Show the gift box at the clicked position
+    giftBox.style.left = `${event.clientX}px`;
+    giftBox.style.top = `${event.clientY}px`;
+
+    showGiftBox();
   });
-  
+});
