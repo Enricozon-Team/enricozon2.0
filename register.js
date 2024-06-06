ee8a451040a0746e911e89738b6276437123f80c
enriccio104 <142581742+enriccio104@users.noreply.github.com>
2024-06-06 19:14:55 +0200

    w

--- register.js
+++ register.js
@@ -1,10 +1,6 @@
 
  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-
-
-
-
  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
  
