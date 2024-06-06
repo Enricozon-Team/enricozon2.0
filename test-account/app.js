b24906d17e1101cb28e635ec9c128cb8f4b70e7f
enriccio104 <142581742+enriccio104@users.noreply.github.com>
2024-06-06 18:52:58 +0200

    c

---
+++ test-account/app.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBHk3SC7rMD5eOl13FDQxGZMNJNTfYScBs",
+   authDomain: "loginnnn-a44e7.firebaseapp.com",
+   projectId: "loginnnn-a44e7",
+   storageBucket: "loginnnn-a44e7.appspot.com",
+   messagingSenderId: "954893313961",
+   appId: "1:954893313961:web:d42e6312c3d8994ed146b8"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
